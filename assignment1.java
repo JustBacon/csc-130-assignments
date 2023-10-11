9523a3b616e3f60cdc5e04a2a18f31180a94ff65
JustBacon <christianbuco17@gmail.com>
2023-10-10 19:35:47 -0700

    doing assignment 2

--- assignment1.java
+++ assignment1.java
@@ -1,29 +1,48 @@
+// Christian Buco
+// Assignment 1
+// CSC 130-05
+// This program assumes that Fibonacci Sequence start at 1 (Classical definition)
+// The other variation is fib(0) = 1 (Combinatorial definition)
+// They can still get 0 if they enter 0
+// If user enters 0 then the number is 0
+// Position 1 is 1 in this program
+// Fibonacci sequence: 0, 1, 1, 2, 3, 5, ...
+
 import java.util.Scanner;
+// import Scanner to get user input
 
 class assignment1 {
 
+    // function used for recursion
     static int fib(int n) {
-
+        // if statement prevents infinite recursion (exit case)
         if (n <= 1)
             return n;
         return fib(n - 1) + fib(n - 2);
     }
 
     public static void main(String[] args) {
-
+        
+        // Create instance of scanner to get user input
         Scanner input = new Scanner(System.in);
-        int n;
+
+        int n_position;
+
+        // ask for user input
         System.out.println("Enter positive integer: ");
+        
+        n_position = input.nextInt();
 
-        n = input.nextInt();
-
-        // input.close();
-        // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
-
+        // get the fibonacci sequence based on user input.
+        // get the next 6 position of the fibonacci number
         for (int i = 0; i < 6; i++) {
+            // record time before the method (startTime)
             long startTime = System.currentTimeMillis();
-            System.out.println(n + "th Fibonacci Number: " + fib(n++));
+            // execute fibonacci method (recursion)
+            System.out.println(n_position + "th Fibonacci Number: " + fib(n_position++));
+            // record time after the method (endTime)
             long endTime = System.currentTimeMillis();
+            // calculate the runtime of the method and display
             long runtime = endTime - startTime;
             System.out.println("Run time: " + runtime + "ms");
         }
--- assignment2.java
+++ assignment2.java
@@ -1,16 +1,24 @@
+// Christian Buco
+// Assignment 2
+// CSC 130-05
+
+// Linear search find BigO(n)
+// Selection sort find BigO(n)
+// Binary search find BigO(n)
+
 import java.util.Scanner;
 
 class assignment2 {
 
-    static int linearSearch(int key, int[] intArray){
+    static int linearSearch(int key, int intArray[]){
         
-        int comparisonCounter = 1;
+        int comparisonCounter = 0;
         int sizeOfArray = intArray.length;
 
         for(int i = 0; i < sizeOfArray; i++){
             if(key == intArray[i]){
                 System.out.println("Number of comparison: " + comparisonCounter);
-                System.out.println("O(" + comparisonCounter + " sizeOfArray)");
+                System.out.println("O(" + comparisonCounter + ")");
                 return i;
             }
             comparisonCounter++;
@@ -18,16 +26,70 @@ class assignment2 {
         return -1;
     }
 
+    public static void swap(int i, int j, int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void selectionSort(int arr[]){
+        int comparisonCounter = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n-1; i++){
+            int minNumIndex = i;
+            for(int j = i+1; j<n;j++){
+                if(arr[j] < arr[minNumIndex])
+                    minNumIndex = j;
+
+                comparisonCounter++;
+            }
+                swap(minNumIndex, i, arr);
+        }
+
+        System.out.println("O(" + comparisonCounter + ")");
+    }
+
+    static int binarySearch(int key, int arr[]){
+        int comparisonCounter = 0;
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int middle = left + (right - 1) / 2;
+
+            if (arr[middle] == key){
+                comparisonCounter++;
+                System.out.println("Number of comparison: " + comparisonCounter);
+                System.out.println("O(" + comparisonCounter + ")");
+                return middle;
+            }
+            
+            if(arr[middle] < key){
+                left = middle + 1;
+                comparisonCounter++;
+            }
+
+            if(arr[middle] > key){
+                right = middle - 1;
+                comparisonCounter++;
+            }
+        }
+
+       return -1;
+    }
+
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
 
-        int[] arrayNum = {
+        int arrayNum[] = {
             3, 7, 8, 2, 13,
             5, 10, 26, 4, 6,
             1, 9, 55, 42, 33,
             11, 16, 0, 21, 87
         };
+
 
         int key = -1;
         int foundIndexNum = -1;
@@ -39,6 +101,25 @@ class assignment2 {
         if(foundIndexNum != -1){
             System.out.println("Found the key at index: " + foundIndexNum);
         }
+        System.out.println();
 
+        input.nextLine();
+        selectionSort(arrayNum);
+
+        for(int i = 0; i<arrayNum.length; i++){
+            System.out.print(arrayNum[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Enter a number inside the array: ");
+        key = input.nextInt();
+
+        foundIndexNum = binarySearch(key, arrayNum);
+        if(foundIndexNum != -1){
+            System.out.println("Found the key at index: " + foundIndexNum);
+        }else{
+            System.out.println("cant find");
+        }
+        System.out.println();
     }
 }
